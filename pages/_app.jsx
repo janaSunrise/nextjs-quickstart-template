78a507131bccd5cd8ab8a0087647ecdcf853edb2
janaSunrise <warriordefenderz@gmail.com>
2021-08-13 17:31:18 +0530

    Add tslib to fix Next-SEO error, and lint files.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,20 @@
-import SEO from '@bradgarropy/next-seo';
-import Layout from '../components/Layout';
+import SEO from "@bradgarropy/next-seo";
+import Layout from "../components/Layout";
 
-import '../styles/globals.scss';
+import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <SEO
-        title='Next JS Quickstart Template'
-        description='A simple and easy boilerplate for your next NextJS project, to get you started in NO time.'
-        keywords={['nextjs', 'react', 'seo', 'web development']}
-        icon='/favicon.ico'
+        title="Next JS Quickstart Template"
+        description="A simple and easy boilerplate for your next NextJS project, to get you started in NO time."
+        keywords={["nextjs", "react", "seo", "web development"]}
+        icon="/favicon.ico"
         facebook={{}}
         twitter={{
-          site: '@JanaSunrise',
-          card: 'summary',
+          site: "@JanaSunrise",
+          card: "summary",
         }}
       />
       <Component {...pageProps} />
